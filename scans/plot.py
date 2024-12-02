8bf50f37873e695b0215e2563581329dca68d3e4
alliec45 <alliec45@vt.edu>
2024-12-02 16:07:08 -0500

    organized unity folder and room class

--- scans/plot.py
+++ scans/plot.py
@@ -13,50 +13,6 @@ Author(s): Allie Craddock, Casie Peng
 """
 ###############
 
-"""
-Plots a time-series model of a singular dataframe for a certain performance indicator. 
-
-Parameters:
-room (Room): A Room object with a str identifier and a list of CSVs of a certain room type 
-num (int): The specific trial number of interest 
-category (str): The specific performance indicator of interest 
-
-"""
-def plot_time_series(room: Room, num: int, category: str):
-    format_ts(room.environment, category)
-
-    df = room.trials[num]
-    df = df.set_index('time')
-    plt.xlim(df.index.min(), df.index.max())
-
-    plt.plot(df.index, df[category])
-
-""" 
-Plots a time-series model of all of the trials associated with an array of dataframes.
-This method does not require the axis parameter. 
-
-Parameters: 
-room (Room): A Room object with a str identifier and the list of CSVs of a certain room type 
-category (str): the performance indicator of interest
-
-"""
-def plot_time_series_trials(room: Room, category: str):   
-    format_ts(room.environment, category)
-
-    min_time = 1000
-    max_time = 0
-
-    for i in range(len(room.trials)):
-        df = room.trials[i].set_index('time')
-        fig, = plt.plot(df.index, df[category])
-        fig.set_label(f'Trial #{i+1}')
-
-        min_time = min(min_time, df.index.min())
-        max_time = max(max_time, df.index.max())
-        
-    plt.xlim(min_time, max_time)
-    plt.legend()
-
 """ 
 This function plots a time-series model of the trials of all room types on subplots 
 for a performance indicator ('category'). 
@@ -68,17 +24,42 @@ category (str): The performance indicator of interest.
 """
 def plot_time_series_all(total_rooms: list, category: str):
     size = len(total_rooms)
-    fig, axes = plt.subplots(1, size, figsize=(25, 5), constrained_layout=True)
-    fig.suptitle(f'{category} vs. Time for Different Groups\n', fontsize=24)
 
-    # Pass the individual axes to each plot
-    for i in range(size):
-        get_time_series_trials(total_rooms[i], category, ax=axes[i])
+    if (size == 0):
+        print("This list is empty")
+    else: 
+        fig, axes = plt.subplots(1, size, figsize=(25, 5), constrained_layout=True)
+        fig.suptitle(f'{category} vs. Time for Different Groups\n', fontsize=24)
 
-        axes[i].set_title((total_rooms[i]).environment, fontsize=20)
-        axes[i].set_xlabel('Time (s)', fontsize=15)
-        axes[i].set_ylabel('Power (mW)', fontsize=15)
+        # Pass the individual axes to each plot
+        for i in range(size):
+            get_time_series_trials(total_rooms[i], category, ax=axes[i])
 
+            axes[i].set_title((total_rooms[i]).environment, fontsize=20)
+            axes[i].set_xlabel('Time (s)', fontsize=15)
+            axes[i].set_ylabel('Power (mW)', fontsize=15)
+
+"""
+Loops through and plots every performance indicator (pi) in a time series (ts) model 
+for all trials for all room types on different subplots. 
+
+Parameters:
+total_rooms (list): A list of Room objects
+
+"""
+def ts_pi(total_rooms: list):
+    size = len(total_rooms)
+
+    if (size == 0):
+        print("This list is empty")
+    else:
+        column_names = list(total_rooms[0].trials[0].columns.values)
+        if "time" in column_names:
+            column_names.remove("time")
+
+        for col in column_names:
+            plot_time_series_all(total_rooms, col)
+            save(f'data_analysis/plots/all_{col}')
 
 """ 
 Plots a time-series model of all of the trials associated with an array of dataframes. 
@@ -104,35 +85,6 @@ def get_time_series_trials(room: Room, category: str, ax: plt.axes):
         
     ax.set_xlim(min_time, max_time)
     ax.legend()
-
-"""
-Plots a boxplot of a singular trial of a single room type. 
-
-Parameters: 
-room (Room): A Room object with a str identifier and a list of CSVs of a certain room type 
-num (int): The specific trial number of interest 
-category (str): The performance indicator of interest
-"""
-def plot_box(room: Room, num: int, category: str):
-    df = room.trials[num]
-    data = df[category].to_numpy()
-    format_bp(room.environment, category)
-    plt.boxplot(data)
-
-"""
-Plots a boxplot of all trials of a singular room type. 
-
-Parameters:
-room (Room): A Room object with a str identifier and a list of CSVs of a certain room type 
-category (str): The performance indicator of interest
-
-"""
-def plot_box_trials(room: Room, category: str):
-    format_bp(room.environment, category)
-
-    for i in range(len(room.trials)):
-        data = [room.trials[i][category].to_numpy()]
-        plt.boxplot(data, positions=[i + 1], labels=[f'Trial #{i+1}'], widths=0.4)
 
 """
 Plot a boxplot for all trials of all room types. 
@@ -185,32 +137,3 @@ path (str): The path to save the figure to.
 def save(path: str):
     plt.savefig(path, bbox_inches='tight')  # Save the figure
     plt.show()  # Display the figure
-
-""" 
-Formats time-series plots. Adjusts size of the plots and font sizes. Adds a title and axes labels. 
-
-Parameters: 
-room_type (str): the room type of the data
-category (str): the performance indicator of interest 
-
-"""
-def format_ts(room_type: str, category: str):
-    plt.figure(figsize=(16, 5))
-    plt.title(f'{room_type}: {category} vs. Time', fontsize=20)
-    plt.xlabel('Time (s)', fontsize=15)
-    plt.ylabel('Power (mW)', fontsize=15)
-    plt.tick_params(axis='x', rotation=30, labelsize=15)
-
-"""
-Formats boxplots. Adjusts the size of the plots and font sizes. Adds a title and axes labels. 
-
-Parameters: 
-room_type (str): the room type of the data
-category (str): the performance indicator of interest 
-
-"""
-def format_bp(room_type: str, category: str):
-    plt.figure(figsize=(10, 5))
-    plt.title(f'{room_type}: {category}', fontsize=20)
-    plt.xlabel(f'{room_type} Scans', fontsize=15)
-    plt.ylabel('Power (mW)', fontsize=15)
